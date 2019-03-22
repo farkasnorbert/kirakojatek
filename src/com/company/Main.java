f8f309cdc38311cbe899dd3e0c2f19a830cbbd91
farkasnorbert <41139862+farkasnorbert@users.noreply.github.com>
2019-03-22 16:10:42 +0200

    Beolvas kiir

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,11 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
+
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Int;
 
 public class Main {
 
@@ -8,9 +13,12 @@ public class Main {
     private static boolean solseq = false;
     private static boolean pcost = false;
     private static boolean nvisited = false;
-    private static int H=1;
+    private static int H=0;
     private static int N;
     private static int M;
+    private static int [][] T;
+    private static int n;
+    private static int [][]Tend;
     public static void main(String[] args) {
         for (int i = 0; i < args.length; i++) {
             String s = args[i];
@@ -53,6 +61,51 @@ public class Main {
                 default:
                     break;
             }
+        }
+    }
+    private static void beolvas(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Tabla merete: ");
+        n = in.nextInt();
+        T=new int [n][n];
+        Tend=new int [n][n];
+        int l=0;
+        for(int i=0; i<n;i++){
+            for(int j=0; j<n; j++){
+                Tend[i][j]=l;
+                l++;
+            }
+        }
+        if(file.equals("")){
+            System.out.println("Kezdoalapot megadasa, 0 az ures");
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                    T[i][j]=in.nextInt();
+                }
+            }
+        }else{
+            try {
+                Scanner sc = new Scanner(new BufferedReader(new FileReader(file)));
+                while(sc.hasNextLine()) {
+                    for (int i=0; i<n; i++) {
+                        String[] line = sc.nextLine().trim().split(" ");
+                        for (int j=0; j<n; j++) {
+                            T[i][j] = Integer.parseInt(line[j]);
+                        }
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private static void kiir(int [][]t, int n){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                System.out.print(t[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
         }
     }
 }
